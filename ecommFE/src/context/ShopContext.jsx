751e85a299d1f463ea8f82f77374a4b221e7eff0
IRjSI <rajusuthar0077@gmail.com>
2025-03-27 20:34:39 +0530

    Add cart functionality with size selection and quantity updates; integrate toast notifications

--- ecommFE/src/context/ShopContext.jsx
+++ ecommFE/src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
+import { toast } from "react-toastify";
 
 export const ShopContext = createContext();
 
@@ -11,8 +12,48 @@ export default function ShopContextProvider({children}) {
     const [cartItems, setCartItems] = useState({});
 
     const addToCart = async (itemId, size) => {
+
+        if (!size) {
+            toast.error('Select Size');
+            return;
+        }
+
         let cartData = structuredClone(cartItems);
-        
+
+        if (cartData[itemId]) {
+            if (cartData[itemId][size]) {
+                cartData[itemId][size] += 1;
+            } else {
+                cartData[itemId][size] = 1;
+            }
+        } else {
+            cartData[itemId] = {};
+            cartData[itemId][size] = 1;
+        }
+
+        setCartItems(cartData);        
+    }
+
+    const getCartCount = () => {
+        let totalCount = 0;
+        for(const items in cartItems) {
+            for (const item in cartItems[items]) {
+                try {
+                    if (cartItems[items][item] > 0) {
+                        totalCount += cartItems[items][item];
+                    }
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+        }
+        return totalCount;
+    }
+
+    const updateQuantity = async (itemId,size,quantity) => {
+        let cartData = structuredClone(cartItems);
+        cartData[itemId][size] = quantity;
+        setCartItems(cartData);
     }
 
     const contextValue = {
@@ -22,7 +63,11 @@ export default function ShopContextProvider({children}) {
         search,
         setSearch,
         showSearch,
-        setShowSearch
+        setShowSearch,
+        addToCart,
+        cartItems,
+        getCartCount,
+        updateQuantity
     }
 
     return (
--- ecommFE/src/pages/Cart.jsx
+++ ecommFE/src/pages/Cart.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Title from '../components/Title';
+import { assets } from '../assets/assets';
 
 function Cart() {
+  const { products, currency, cartItems, updateQuantity } = useContext(ShopContext);
+
+  const [cartData,setCartData] = useState([]);
+
+  useEffect(() => {
+    let tempData = [];
+
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        if (cartItems[items][item] > 0) {
+          
+          tempData.push({
+            _id:items,
+            size:item,
+            quantity: cartItems[items][item]
+          })
+        }
+      }
+    }
+    setCartData(tempData);
+  }, [cartItems])
+
   return (
-    <div>
-      
+    <div className='border-t pt-14'>
+      <div className='text-2xl mb-3'>
+        <Title text1={'YOUR'} text2={'CART'} />
+      </div>
+      <div>
+        {
+          cartData.map((item,ind) => {
+            const productData = products.find((product) => product._id === item._id);
+
+            return (
+              <div key={ind} className='py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4'>
+                <div className='flex items-start gap-6'>
+                  <img src={productData.image[0]} className='w-16 sm:w-20' alt="" />
+                  <div>
+                    <p className='text-sm font-medium sm:text-lg'>{productData.name}</p>
+                    <div className='flex items-center gap-5 mt-2'>
+                      <p>{currency}{productData.price}</p>
+                      <p>{item.size}</p>
+                    </div>
+                  </div>
+                </div>
+                <input type="number" min={1} defaultValue={item.quantity} onChange={(e) => e.target.value === '' || e.target.value === '0' ? null : updateQuantity(item._id,item.size,e.target.value)} />
+                <img onClick={() => updateQuantity(item._id,item.size,0)} src={assets.bin_icon} className='cursor-pointer w-4 mr-4 sm:w-5 ' alt="" />
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
